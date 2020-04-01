18ca8762e9acf5a7e13d54da85edb30b67a74154
Dilip Sundarraj <dilip.sundarraj@target.com>
2020-04-01 09:20:45 -0500

    added the axios instead of the fetch

--- src/App.js
+++ src/App.js
@@ -1,47 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
 
-  state = {
-    file: '',
-    error: '',
-    msg: ''
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null,
+        error: '',
+        msg: ''
+      }
+   
   }
 
   uploadFile = (event) => {
-    event.preventDefault();
+    //event.preventDefault();
     this.setState({error: '', msg: ''});
 
-    if(!this.state.file) {
+    if(!this.state.selectedFile) {
       this.setState({error: 'Please upload a file.'})
       return;
     }
 
-    if(this.state.file.size >= 2000000) {
+    if(this.state.selectedFile.size >= 2000000) {
       this.setState({error: 'File size exceeds limit of 2MB.'})
       return;
     }
 
     let data = new FormData();
-    data.append('file', this.state.file);
-    data.append('name', this.state.file.name);
-
-    fetch('http://localhost:8080/api/files', {
-      method: 'POST',
-      body: data
-    }).then(response => {
-      this.setState({error: '', msg: 'Sucessfully uploaded file'});
-    }).catch(err => {
-      this.setState({error: err});
-    });
-
-  }
+    data.append('file', this.state.selectedFile);
+    data.append('name', this.state.selectedFile.name);
+    data.append('randomString', "{\"name\":\"dilip\"}");
+    axios.post("http://localhost:8080/fileUpload", data)
+  .then(res => { // then print response status
+    console.log(res)
+    this.setState({
+      msg: 'File Uploaded Successfully'
+    })
+  })
+  .catch(err => { // then print response status
+    console.log(err)
+    this.setState({
+      error: err.statusText
+    })
+  })
+}
 
   onFileChange = (event) => {
     this.setState({
       file: event.target.files[0]
+    });
+  }
+
+  onFileChangeHandler = (event) => {
+    this.setState({
+      selectedFile: event.target.files[0]
     });
   }
 
@@ -52,7 +68,9 @@ class App extends Component {
           <h3>Upload a file</h3>
           <h4 style={{color: 'red'}}>{this.state.error}</h4>
           <h4 style={{color: 'green'}}>{this.state.msg}</h4>
-          <input onChange={this.onFileChange} type="file"></input>
+          {/* <input onChange={this.onFileChange} type="file"></input>*/}
+          <input onChange={this.onFileChangeHandler} type="file"></input>
+
           <button onClick={this.uploadFile}>Upload</button>   
         </div>
       </div>
